c6dd0278640ebe27b1f5fdc35dc20ccac7b161e4
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-01-11 13:11:43 -0500

    Groovy representation element filter

---
+++ bob-decorator.groovy
@@ -0,0 +1,27 @@
+import org.nasdanika.models.app.graph.drawio.RepresentationElementFilter;
+import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.drawio.Element;
+import org.nasdanika.drawio.ModelElement;
+import org.nasdanika.graph.processor.ProcessorInfo;
+import org.nasdanika.models.app.graph.WidgetFactory;
+import org.nasdanika.capability.CapabilityFactory.Loader
+
+// Script arguments for reference
+Loader loader = args[0];
+ProgressMonitor loaderProgressMonitor = args[1];
+Object data = args[2]; // From fragment
+
+new RepresentationElementFilter() {
+
+    @Override
+    void filterRepresentationElement(
+            ModelElement sourceElement, 
+            ModelElement representationElement,
+            Map<Element, ProcessorInfo<WidgetFactory>> registry, ProgressMonitor progressMonitor) {
+
+        if ("Bob".equals(representationElement.getLabel())) {
+            representationElement.style("imageBorder", "default");
+        }
+    }
+    
+}
